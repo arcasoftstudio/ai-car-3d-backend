78356fbde2491941308f26769508fc0436e202e1
arcasoftstudio <arcasoftstudio@gmail.com>
2025-04-23 20:47:57 +0300

    Update build_meshroom.sh

--- build_meshroom.sh
+++ build_meshroom.sh
@@ -8,14 +8,14 @@ ln -fs /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
 echo "🔧 Aggiorno e installo dipendenze..."
 apt update && apt install -y tzdata && dpkg-reconfigure -f noninteractive tzdata
-apt update && apt install -y \
-    build-essential cmake git wget curl unzip \
+apt install -y \
+    git cmake build-essential \
     libboost-all-dev libboost-system-dev libboost-thread-dev \
     libeigen3-dev libopenimageio-dev openimageio-tools \
     libpng-dev libjpeg-dev libtiff-dev libraw-dev libgl1 libglu1-mesa-dev \
     libopenexr-dev libglew-dev libglib2.0-0 libopencv-dev \
     qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools \
-    python3-pip zlib1g-dev pkg-config tzdata
+    python3-pip zlib1g-dev pkg-config
 
 echo "🐙 Clono AliceVision..."
 git clone --recursive https://github.com/alicevision/AliceVision.git
@@ -30,8 +30,13 @@ echo "🔧 Disattivo check nanoflann nel CMakeLists.txt"
 sed -i '/find_package(nanoflann REQUIRED)/d' ../src/CMakeLists.txt
 sed -i '/message(FATAL_ERROR "Failed to find nanoflann.")/d' ../src/CMakeLists.txt
 
-echo "🛠️ Compilo AliceVision"
-cmake .. -DCMAKE_BUILD_TYPE=Release -DALICEVISION_USE_CUDA=ON
+echo "🛠️ Compilo AliceVision con Boost forzato"
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+         -DALICEVISION_USE_CUDA=ON \
+         -DBoost_NO_BOOST_CMAKE=ON \
+         -DBOOST_ROOT=/usr/include \
+         -DBoost_INCLUDE_DIR=/usr/include
+
 make -j$(nproc)
 
 echo "✅ AliceVision compilato!"
